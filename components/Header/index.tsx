9b9e2efb90d3f634561c7c8828b877cf58eadee6
nakachan-ing <79064717+nakachan-ing@users.noreply.github.com>
2023-09-28 23:27:01 +0900

    💄 Headerのロゴを変更

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -6,12 +6,20 @@ export default function Header() {
   return (
     <header className={styles.header}>
       <Link href="/">
-        <Image
+        {/* <Image
           src="/logo.svg"
           alt="SIMPLE"
           className={styles.logo}
           width={348}
           height={133}
+          priority
+        /> */}
+        <Image
+          src="https://s4.aconvert.com/convert/p3r68-cdx67/aauh1-ezfrd.svg"
+          alt="Kyonaka Blog"
+          className={styles.logo}
+          width={500}
+          height={500}
           priority
         />
       </Link>
